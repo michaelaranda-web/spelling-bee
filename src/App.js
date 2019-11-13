88666e2809d7d87eebb097d9302aa0ef72fd82d4
EC2 Default User <ec2-user@ip-172-31-16-143.us-west-2.compute.internal>
2019-11-13 09:14:04 +0000

    Add ability to type in letters and submit words. List found words.

--- src/App.js
+++ src/App.js
@@ -12,13 +12,23 @@ class App extends React.Component {
     this.fetchPuzzleData = this.fetchPuzzleData.bind(this);
     
     this.state = {
+      currentInput: "",
       validWords: [],
+      foundWords: [],
       centerLetter: '',
       outerLetters: [],
     }
     
     this.fetchPuzzleData();
   }
+  
+  componentWillMount() {
+      document.addEventListener("keydown", this.onKeyPress.bind(this));
+  }
+
+  componentWillUnmount() {
+      document.removeEventListener("keydown", this.onKeyPress.bind(this));
+  } 
   
   async fetchPuzzleData() {
     fetch('https://cors-anywhere.herokuapp.com/' + testURL)
@@ -35,29 +45,68 @@ class App extends React.Component {
       });
   }
   
+  submitWord() {
+    if (this.state.validWords.indexOf(this.state.currentInput) > -1) {
+      let newFoundWords = [...this.state.foundWords, this.state.currentInput]
+      this.setState({
+        foundWords: newFoundWords
+      })
+    }
+  }
+  
+  onKeyPress(e) {
+    if (e.key.search(/^[a-zA-Z]$/) > -1) {
+      this.setState({
+        currentInput: this.state.currentInput + e.key
+      });
+    } else if (e.key === 'Backspace') {
+      this.setState({
+        currentInput: this.state.currentInput.slice(0, this.state.currentInput.length - 1) 
+      });
+    } else if (e.key === 'Enter') {
+      this.submitWord();
+      this.setState({
+        currentInput: ""
+      })
+    }
+  }
+  
   render() {
     return (
       <div className="App">
         <h1>Superior Spelling Bee App</h1>
         
         <div id="">
-          <input 
-            autoFocus
-          />
+          <div className="sb-hive-input">
+            <input 
+              className="sb-hive-input-content"
+              value={this.state.currentInput}
+            />
+          </div>
         </div>
         
         <br />
         
-        <div class="sb-hive">
-          <div class="hive">
-            <HiveCell letter={this.state.centerLetter} cellType={'center'} />
-            
-            {
-              this.state.outerLetters.map((letter, i) => {
-                return <HiveCell key={i} letter={letter} cellType={'outer'} />
-              })
-            }
+        <div className="temporary-hive-container">
+          <div className="sb-hive">
+            <div className="hive">
+              <HiveCell letter={this.state.centerLetter} cellType={'center'} />
+              
+              {
+                this.state.outerLetters.map((letter, i) => {
+                  return <HiveCell key={i} letter={letter} cellType={'outer'} />
+                })
+              }
+            </div>
           </div>
+        </div>
+        
+        <div id="found-words-section">
+          {
+            this.state.foundWords.map((foundWord, i) => {
+              return <p key={i}>{foundWord}</p>
+            })
+          }
         </div>
       </div>
     );
--- src/components/HiveCell.js
+++ src/components/HiveCell.js
@@ -3,15 +3,15 @@ import React from 'react';
 export function HiveCell(props) {
   return (
     <svg 
-      class={`hive-cell ${props.cellType}`} 
+      className={`hive-cell ${props.cellType}`} 
       viewBox="0 0 120 103.92304845413263">
       <polygon 
-        class="cell-fill" 
+        className="cell-fill" 
         points="0,51.96152422706631 30,0 90,0 120,51.96152422706631 90,103.92304845413263 30,103.92304845413263" 
         stroke="white" 
-        stroke-width="7.5">
+        strokeWidth="7.5">
       </polygon>
-      <text class="cell-letter" x="50%" y="50%" dy="10.75%">{props.letter}</text>
+      <text className="cell-letter" x="50%" y="50%" dy="10.75%">{props.letter}</text>
     </svg>
   );
 }
