f9355c3aed0e67e51d589ad4077d5c936f55551e
EC2 Default User <ec2-user@ip-172-31-16-143.us-west-2.compute.internal>
2019-11-14 08:57:27 +0000

    Adds animated score update alert.

--- src/components/ScoreBar.js
+++ src/components/ScoreBar.js
@@ -1,16 +1,52 @@
 import React from 'react';
 
-export function ScoreBar(props) {
-  return (
-    <div id="score-bar">
-      <div className="score-row">
-        <span className="score-icon current-score-icon"></span><span className="score-value">{props.currentScore}</span>
+export class ScoreBar extends React.Component {
+  constructor(props)  {
+    super(props);
+    
+    this.state = {
+      pointsUpdate: 0,
+      showPointsUpdate: false,
+    }
+  }
+  
+  componentDidUpdate(prevProps) {
+    if (prevProps.currentScore < this.props.currentScore) {
+      const pointsUpdate = this.props.currentScore - prevProps.currentScore;
+      
+      this.setState({
+        pointsUpdate: pointsUpdate,
+        showPointsUpdate: true
+      });
+      
+      setTimeout(() => {this.setState({showPointsUpdate: false})}, 1500);
+    }
+  }
+  
+  renderPointsUpdate() {
+    if (this.state.showPointsUpdate) {
+      return (
+        <div className="points-update">
+          <div className="points-value">{this.state.pointsUpdate}</div>
+        </div>  
+      );
+    }
+  }
+  
+  render() {
+    return (
+      <div id="score-bar">
+        <div className="score-row">
+          <span className="score-icon current-score-icon"></span><span className="score-value">{this.props.currentScore}</span>
+          {this.renderPointsUpdate()}
+        </div>
+        <div className="score-row">
+          <span className="score-icon genius-score-icon"></span>
+          <span className="score-value">{this.props.geniusScore}</span>
+        </div>
       </div>
-      <div className="score-row">
-        <span className="score-icon genius-score-icon"></span><span className="score-value">{props.geniusScore}</span>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ScoreBar;
