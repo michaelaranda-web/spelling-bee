b9be61691e500f052bcc8b7caca499ba73a65e8e
EC2 Default User <ec2-user@ip-172-31-16-143.us-west-2.compute.internal>
2019-11-20 06:10:34 +0000

    More UI fixes.

--- src/components/WordInput.js
+++ src/components/WordInput.js
@@ -4,13 +4,13 @@ export const WordInput = (props) => {
   let currentInputLetters = props.currentInput.split('');
   let puzzleLetters = [...props.outerLetters, props.centerLetter];
   
-  let inputDisplay = currentInputLetters.map((inputLetter) => {
+  let inputDisplay = currentInputLetters.map((inputLetter, i) => {
     if (puzzleLetters.indexOf(inputLetter) === -1) {
-      return <span className="invalid-letter">{inputLetter}</span>
+      return <span className="invalid-letter" key={i}>{inputLetter}</span>
     } else if (props.centerLetter === inputLetter) {
-      return <span className="center-letter">{inputLetter}</span>
+      return <span className="center-letter" key={i}>{inputLetter}</span>
     } else {
-      return <span className="input-letter">{inputLetter}</span>
+      return <span className="input-letter" key={i}>{inputLetter}</span>
     }
   });
   
