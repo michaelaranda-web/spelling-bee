ebe57e76094e18926b88e2375e3d079349bfb10a
EC2 Default User <ec2-user@ip-172-31-16-143.us-west-2.compute.internal>
2019-12-02 19:29:33 +0000

    Adds button feedback css.

--- src/components/HiveCell.js
+++ src/components/HiveCell.js
@@ -1,19 +1,52 @@
 import React from 'react';
 
-export function HiveCell(props) {
-  return (
-    <svg 
-      className={`hive-cell ${props.cellType}`} 
-      onClick={() => props.onClick(props.letter)}
-      viewBox="0 0 120 103.92304845413263">
-      <polygon 
-        className="cell-fill" 
-        points="0,51.96152422706631 30,0 90,0 120,51.96152422706631 90,103.92304845413263 30,103.92304845413263" 
-        stroke="white" 
-        strokeWidth="7.5">
-      </polygon>
-      <text className="cell-letter" x="50%" y="50%" dy="10.75%">{props.letter}</text>
-    </svg>
-  );
+export class HiveCell extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      pushActive: false
+    };
+  }
+  
+  cellFillClass() {
+    if (this.state.pushActive) {
+      return `cell-fill ${this.props.cellType} push-active`;
+    } else {
+      return `cell-fill ${this.props.cellType}`;
+    }
+  }
+  
+  onMouseDown() {
+    this.setState({
+      pushActive: true
+    });
+    
+    this.props.onClick(this.props.letter);
+  }
+  
+  onMouseUp() {
+    this.setState({
+      pushActive: false
+    });
+  }
+  
+  render() {
+    return (
+      <svg 
+        className={`hive-cell ${this.props.cellType}`} 
+        onMouseDown={this.onMouseDown.bind(this)}
+        onMouseUp={this.onMouseUp.bind(this)}
+        viewBox="0 0 120 103.92304845413263">
+        <polygon 
+          className={this.cellFillClass()} 
+          points="0,51.96152422706631 30,0 90,0 120,51.96152422706631 90,103.92304845413263 30,103.92304845413263" 
+          stroke="white" 
+          strokeWidth="7.5">
+        </polygon>
+        <text className="cell-letter" x="50%" y="50%" dy="10.75%">{this.props.letter}</text>
+      </svg>
+    );
+  }
 }
 
