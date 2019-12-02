7df6b17d44d32cc8e69c289415cd6791ec0892da
EC2 Default User <ec2-user@ip-172-31-16-143.us-west-2.compute.internal>
2019-12-02 19:29:07 +0000

    WIP performance optimization test.

--- src/components/FoundWords.js
+++ src/components/FoundWords.js
@@ -31,14 +31,6 @@ export const FoundWords = (props) => {
       <ul id="found-words-list-box">
         {
           props.foundWords.sort().map((foundWord) => {
-            if (props.currentInput !== "") {
-              if (foundWord.startsWith(props.currentInput)) {
-                return <FoundWordItem key={foundWord} componentClass="found-words-list-item" word={foundWord} />
-              } else {
-                return <FoundWordItem key={foundWord} componentClass="found-words-list-item grey-out" word={foundWord} />
-              }
-            }
-            
             return <FoundWordItem key={foundWord} componentClass="found-words-list-item" word={foundWord} />
           })
         }
