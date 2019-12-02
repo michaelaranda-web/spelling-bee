caef0d60ef3cfe94b9085e092cb78833d3ffbabc
EC2 Default User <ec2-user@ip-172-31-16-143.us-west-2.compute.internal>
2019-12-02 20:16:42 +0000

    Attemp #3 to fix button responsiveness for mobile.

--- src/components/HiveCell.js
+++ src/components/HiveCell.js
@@ -37,11 +37,15 @@ export class HiveCell extends React.Component {
         className={`hive-cell ${this.props.cellType}`} 
         onMouseDown={this.onMouseDown.bind(this)}
         onMouseUp={this.onMouseUp.bind(this)}
+        onTouchStart={this.onMouseDown.bind(this)}
+        onTouchStop={this.onMouseUp.bind(this)}
         viewBox="0 0 120 103.92304845413263">
         <polygon 
           className={this.cellFillClass()} 
           onMouseDown={this.onMouseDown.bind(this)}
           onMouseUp={this.onMouseUp.bind(this)}
+          onTouchStart={this.onMouseDown.bind(this)}
+          onTouchStop={this.onMouseUp.bind(this)}
           points="0,51.96152422706631 30,0 90,0 120,51.96152422706631 90,103.92304845413263 30,103.92304845413263" 
           stroke="white" 
           strokeWidth="7.5">
@@ -49,6 +53,8 @@ export class HiveCell extends React.Component {
         <text
           onMouseDown={this.onMouseDown.bind(this)}
           onMouseUp={this.onMouseUp.bind(this)}
+          onTouchStart={this.onMouseDown.bind(this)}
+          onTouchStop={this.onMouseUp.bind(this)}
           className="cell-letter" x="50%" y="50%" dy="10.75%">{this.props.letter}</text>
       </svg>
     );
